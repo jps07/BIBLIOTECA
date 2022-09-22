d3adbef8c8ef1816629022aa1b96b54b9bfafd15
jps07 <javega165@misena.edu.co>
2022-09-22 17:49:34 -0500

    se levanto servidor

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aplicación con Node yExpress' });
+  res.render('index', { title: 'Aplicación' });
 });
 
 module.exports = router;
