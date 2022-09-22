b2d3ac61a304c47eb530fc83a9f253c5fed731b4
jps07 <javega165@misena.edu.co>
2022-09-22 18:33:32 -0500

    se crea archivo de controlador para trabajar modelo vista controlador

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+//se cambia ruta en index.
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('libros/index', { title: 'Aplicación' });
