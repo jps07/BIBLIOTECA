58b08b5e9d5dfc17790c130b86d62d13747ef026
jps07 <javega165@misena.edu.co>
2022-09-22 21:32:32 -0500

    se cambia la ruta para acceder desde libros controller

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const librosController= require( "../controllers/librosController");
 //se cambia ruta en index.
 /* GET home page. */
-router.get('/',librosController);
+router.get('/',librosController.index);
 
 module.exports = router;
