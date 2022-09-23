0499642e50bd093abd61da7a900c65ca021c3cd4
jps07 <javega165@misena.edu.co>
2022-09-23 17:10:53 -0500

    se da mas espacio

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -3,12 +3,13 @@ var con = require('../config/conexion');
 
 module.exports={
 
+    
     index: function (req, res) {
 
         con.query("SELECT * FROM libros", function (err, datos) {
             console.log(datos);
         });
-        
+
         res.render('libros/index', { title: 'Aplicación' });
     }
 }   
