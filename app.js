8b15cc52c4e8ed4b4979f71516fb6435b8c56b85
jps07 <javega165@misena.edu.co>
2022-09-23 20:27:42 -0500

    se quita el const

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//se configura ruta en usuarios para permitir el acceso direto a libros route.
-app.use('/libros', usersRouter);
+//se configura ruta en usuarios para permitir el acceso direto a libros router.
+app.use('/libros', librosRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-const librosController= require( "../controllers/librosController");
+//aca se mostraria el log in.
 //se cambia ruta en index.
 /* GET home page. */
-router.get('/',librosController.index);
+router.get('/',function (req,res,next) {
+    res.send("Bienvenido a la biblioteca")
+});
 
 module.exports = router;
