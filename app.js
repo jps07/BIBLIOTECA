9cfb2eced3a683fe9b5badfd1c1ddb5d24f35de4
jps07 <javega165@misena.edu.co>
2022-09-23 19:51:56 -0500

    se añade comentario

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//se configura ruta en usuarios para permitir el acceso direto a libros route.
 app.use('/libros', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
